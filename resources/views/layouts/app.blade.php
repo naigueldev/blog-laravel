c25ab21095fc5586837b311e69d2c4b8f09bc414
Naiguel Correa dos santos <naiguelsantos@iMac-de-Naiguel.local>
2019-11-19 01:03:56 -0300

    create pages for post

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>BLOG APP</title>
+    
+    {{-- <link rel="stylesheet" href="{{asset('css/app.css')}}"> --}}
+    
+</head>
+<body>
+    @include('inc.navbar')
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <h1>Index Page</h1>
+@endsection
