8871a4d24bc17af5ab833ae34f349603bc043206
Naiguel Correa dos santos <naiguelsantos@iMac-de-Naiguel.local>
2019-11-20 17:59:10 -0300

    create authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,32 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <title>BLOG APP</title>
-    
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
     <link rel="stylesheet" href="{{asset('scss/app.scss')}}">
-    
 </head>
 <body>
-    @include('inc.navbar')
-    <main role="main" class="container">
-        @include('inc.messages')
-        @yield('content')
-    </main>
-    
+    <div id="app">
+        @include('inc.navbar')
+        <main role="main" class="container py-4">
+            @include('inc.messages')
+            @yield('content')
+        </main>
+
+    </div>
 </body>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,12 +7,20 @@
     
     <h1>{{$post->title}}</h1>
     
-    <small>{{$post->created_at}}</small>
-    
     <div class="body-content py-5">
         <p>{{$post->body}}</p>
     </div>
-    
+
+    <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <div class="d-flex justify-content-center">
+        <a href="/posts/{{$post->id}}/edit" class="btn btn-default mx-4">Edit</a>
+        {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'form-delete-class' ])!!}
+            {{Form::hidden('_method','DELETE')}}
+            {{Form::submit('Delete', ['class' => 'btn btn-danger mx-3'])}}
+        {!!Form::close()!!}
+
+    </div>
 </div>
 
 @endsection
